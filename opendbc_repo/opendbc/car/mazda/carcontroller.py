b4900d9782c57644235ed1148b0588cceff142e9
bryan065 <le.bryan.065@gmail.com>
2026-08-11 14:57:00 -0400

    test HUD alert supression2

--- opendbc_repo/opendbc/car/mazda/carcontroller.py
+++ opendbc_repo/opendbc/car/mazda/carcontroller.py
@@ -85,7 +85,14 @@ class CarController(CarControllerBase, IntelligentCruiseButtonManagementInterfac
       # TODO: find a way to silence audible warnings so we can add more hud alerts
 
       # supress HUD warnings for steering limits
-      if steer_required:
+      # Check for steersaturated events
+      steer_saturated_event = any(
+          e.name == "steerSaturated"
+          for e in CC.events
+      )
+
+      # supress HUD warnings for steering limits
+      if steer_required or steer_saturated_event:
         steer_required == False
 
       steer_required = steer_required and CS.lkas_allowed_speed
